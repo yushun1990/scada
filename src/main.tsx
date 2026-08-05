ccb9756bec116e5b96db1a1d66010013a9f37b27
yushun1990 <yushun1990@gmail.com>
2026-08-05 21:22:02 +0800

    chore: avoid duplicate M2 stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
-import './m2.css'
 
 const rootElement = document.getElementById('root')
 
