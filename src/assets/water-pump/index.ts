878b562fcf76402a52258adbd73c750f9778d6d6
yushun1990 <yushun1990@gmail.com>
2026-08-05 16:06:13 +0800

    feat: assemble pump SVG asset

---
+++ src/assets/water-pump/index.ts
@@ -0,0 +1,12 @@
+import part00 from './part-00.txt?raw'
+import part01 from './part-01.txt?raw'
+import part02 from './part-02.txt?raw'
+import part03 from './part-03.txt?raw'
+import part04 from './part-04.txt?raw'
+import part05 from './part-05.txt?raw'
+import part06 from './part-06.txt?raw'
+import part07 from './part-07.txt?raw'
+
+const pumpSvg = [part00, part01, part02, part03, part04, part05, part06, part07].join('')
+
+export default pumpSvg
