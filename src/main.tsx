bdae62b8cfd9566c152fd2d1309f2118c8b4a218
yushun1990 <yushun1990@gmail.com>
2026-08-05 16:08:27 +0800

    style: load pump demo stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './pump.css'
 
 const rootElement = document.getElementById('root')
 
