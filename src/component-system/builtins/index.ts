846f020ddbc164200bb80b583015858955188c72
yushun1990 <yushun1990@gmail.com>
2026-08-08 12:28:45 +0800

    feat: register status indicator component

--- src/component-system/builtins/index.ts
+++ src/component-system/builtins/index.ts
@@ -1,8 +1,19 @@
 import { ComponentRegistry } from '../registry'
 import { pumpComponentRegistration } from './pump'
+import { statusIndicatorComponentRegistration } from './status-indicator'
 
 export const builtInComponentRegistry = new ComponentRegistry([
   pumpComponentRegistration,
+  statusIndicatorComponentRegistration,
 ])
 
-export { pumpComponentDefinition, pumpComponentRegistration, PUMP_COMPONENT_TYPE } from './pump'
+export {
+  pumpComponentDefinition,
+  pumpComponentRegistration,
+  PUMP_COMPONENT_TYPE,
+} from './pump'
+export {
+  statusIndicatorComponentDefinition,
+  statusIndicatorComponentRegistration,
+  STATUS_INDICATOR_COMPONENT_TYPE,
+} from './status-indicator'
