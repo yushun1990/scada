2b67701e5b7b66dd9861e2f3eb3932b4241224b2
yushun1990 <yushun1990@gmail.com>
2026-09-11 19:58:04 +0800

    feat: export C1 Studio primitives

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,12 +1,29 @@
 export { Button, type StudioButtonProps, type StudioButtonVariant } from './Button'
 export { Checkbox, type CheckboxProps } from './Checkbox'
+export {
+  DialogRoot,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogClose,
+} from './Dialog'
 export { IconButton } from './IconButton'
 export { Input, type InputProps } from './Input'
+export {
+  MenuRoot,
+  MenuTrigger,
+  MenuPopup,
+  MenuItem,
+  MenuSeparator,
+} from './Menu'
 export { NumberInput, type NumberInputProps } from './NumberInput'
 export { Pressable, type PressableProps } from './Pressable'
 export { SegmentedControl, type SegmentedControlItem } from './SegmentedControl'
 export { Select, type SelectOption, type SelectProps } from './Select'
 export { Separator } from './Separator'
+export { SplitPane } from './SplitPane'
+export { StatusBar, type StatusBarProps } from './StatusBar'
 export { Tabs, type StudioTabItem } from './Tabs'
 export { Textarea, type TextareaProps } from './Textarea'
 export { Toolbar, ToolbarButton, ToolbarGroup, ToolbarSeparator } from './Toolbar'
